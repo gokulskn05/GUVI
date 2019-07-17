f9c0267a0d215b74d36e6c0abad0723b0bf97972
gokulskn05 <42958477+gokulskn05@users.noreply.github.com>
2019-07-18 00:01:30 +0530

    Update OddEven.java

--- Beginner/OddEven.java
+++ Beginner/OddEven.java
@@ -5,9 +5,12 @@ class oddEven{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        if (n % 2 == 0)
-            System.out.println("Even");
+        if(a>=0)
+            if (n % 2 == 0)
+                System.out.println("Even");
+            else
+                System.out.println("Odd");
         else
-            System.out.println("Odd");
+            System.out.println("Invalid");
     }
 }
