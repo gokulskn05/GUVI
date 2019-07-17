1e936b52417694ce0eed5db132fe1535dbc26505
gokulskn05 <42958477+gokulskn05@users.noreply.github.com>
2019-07-17 23:58:41 +0530

    Create OddEven.java

---
+++ Beginner/OddEven.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class oddEven{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
